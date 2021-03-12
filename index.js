db04fa428f20d72dbc4ea75b95a9edf2e5bf0332
mysteriou <elio007@hotmail.fr>
2021-03-12 17:35:36 +0100

    ajout router dans ./index.js

--- index.js
+++ index.js
@@ -1,1 +1,22 @@
+const Foo = { template: '<div>foo</div>' }
+const projet = {template: '<div>projet</div>'}
+
+
+const routes = [
+        
+	{path:'/', component: projet},
+	{path:'/projet', component: Foo}
+
+]
+
+  const router =  new VueRouter({
+  
+  routes
+  
+  })
+
+ const app = new Vue({
  
+ router
+ 
+ }).$mount('#app')
