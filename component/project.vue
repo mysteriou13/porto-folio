9c95c55524080e26867c7e31c00241470e536a2b
mysteriou <elio007@hotmail.fr>
2021-03-12 18:41:46 +0100

    ajout httpvueLoader dans ./index.html

---
+++ component/project.vue
@@ -0,0 +1,3 @@
+<template>
+	<div> projet vue</div>
+	</template>
--- index.js
+++ index.js
@@ -1,11 +1,7 @@
-const Foo = { template: '<div>foo</div>' }
-const projet = {template: '<div>projet</div>'}
-
-
 const routes = [
         
-	{path:'/', component: projet},
-	{path:'/projet', component: Foo}
+	{path:'/', component:  httpVueLoader('./component/home.vue')},
+	{path:'/projet', component: httpVueLoader('./component/project.vue')}
 
 ]
 
